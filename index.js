afe879db32f02d88bbd7c8be0fd5210eaa1446ec
jiaqiu <jiaqiu.win@outlook.com>
2023-09-11 15:08:05 +0800

    pipe operations

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const { Observable } = require('rxjs');
+const { map } = require('rxjs/operators');
+const user = {
+    data: [
+        {
+            status: 'inactive',
+            age: 11
+        },
+        {
+            status: 'active',
+            age: 17
+        },
+        {
+            status: 'active',
+            age: 45
+        },
+        {
+            status: 'inactive',
+            age: 51
+        },
+    ]
+}
 
 // create Observable
 const observable = new Observable((subscriber) => {
-    subscriber.next(10);
-    subscriber.next(11);
-    subscriber.next(12);
-})
+    subscriber.next(user);
+}).pipe(
+    map((data) => {
+        console.log('data from Observable', data)
+        return data.data
+    }),
+    map((data) => {
+        console.log('data from the first operator', data)
+        return data.filter(s => s.status === 'active')
+    }),
+    map((data) => {
+        console.log('data from the second operator', data)
+        return data.reduce((prev, curr) => {
+            return prev + curr.age
+        }, 0)
+    })
+)
 
 // create observer
 const observer = {
